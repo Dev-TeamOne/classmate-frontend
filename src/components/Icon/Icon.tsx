6055c5be513388a72824ff473293d55c473c6e70
HyewonKkang <68578916+HyewonKkang@users.noreply.github.com>
2023-03-20 14:34:59 +0900

    feat: 공통 style 및 Icon, Input 컴포넌트 추가
    * assets 파일 추가
    * box style 정의
    * Icon 컴포넌트 구현
    * Input(TextField, Textarea) 컴포넌트 구현

---
+++ src/components/Icon/Icon.tsx
@@ -0,0 +1,56 @@
+import React, { HTMLAttributes } from 'react';
+import styled from 'styled-components';
+import useDynamicSVGImport from '../../hooks/useDynamicSvgImport';
+import { colors, ColorType } from '../../styles/theme';
+
+export interface Props extends HTMLAttributes<HTMLSpanElement> {
+  name: string;
+  color?: keyof ColorType;
+  size?: number;
+}
+
+function Icon({ name, color, size = 24, style, ...rest }: Props) {
+  const iconSize = `${size}px`;
+  const { SvgIcon } = useDynamicSVGImport({ name });
+
+  return (
+    <IconWrapper
+      style={{
+        width: iconSize,
+        minWidth: iconSize,
+        ...style,
+      }}
+      {...rest}
+    >
+      {SvgIcon ? (
+        <SvgIcon
+          style={{ width: '100%', height: '100%', color: color ? colors[color] : undefined }}
+        />
+      ) : null}
+    </IconWrapper>
+  );
+}
+
+export default Icon;
+
+const IconWrapper = styled.span`
+  display: inline-block;
+  line-height: 0;
+
+  svg {
+    width: 100%;
+    height: 100%;
+
+    [fill] {
+      &:not([fill='none']) {
+        fill: currentColor;
+      }
+    }
+
+    [stroke] {
+      &:not([stroke='none']) {
+        stroke: currentColor;
+      }
+    }
+  }
+`;
---
+++ src/components/Input/Input.tsx
@@ -0,0 +1,12 @@
+import TextArea, { MultiLineInputProps } from './Textarea';
+import TextField, { SingleLineInputProps } from './TextField';
+
+function Input({ multiline = false, ...props }: SingleLineInputProps | MultiLineInputProps) {
+  if (multiline) {
+    return TextArea(props as MultiLineInputProps);
+  } else {
+    return TextField(props as SingleLineInputProps);
+  }
+}
+
+export default Input;

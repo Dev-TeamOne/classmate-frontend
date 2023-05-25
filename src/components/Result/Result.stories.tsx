1de2edc5cb0340f3b686993d3ea51dedbf43d1e5
HyewonKkang <68578916+HyewonKkang@users.noreply.github.com>
2023-05-25 12:51:48 +0900

    feat: Result 컴포넌트 추가

---
+++ src/components/Result/Result.stories.tsx
@@ -0,0 +1,66 @@
+import { withKnobs, text, select, boolean } from '@storybook/addon-knobs';
+import Result from './Result';
+import { action } from '@storybook/addon-actions';
+import Flex from '../Flex/Flex';
+import Button from '../Button';
+
+/**
+ * Result는 일련의 운영 작업 결과를 피드백하는 데 사용됩니다.
+ *
+ * 중요한 작업이 결과를 처리하도록 사용자에게 알려야 하고 피드백이 더 복잡할 때 사용합니다.
+ * Result의 사이즈는 부모 요소에 fit하게 들어가며 사용자 지정 사이즈 설정 시 style 속성을 추가해야 합니다.
+ */
+
+export default {
+  title: 'Components/Result',
+  decorators: [withKnobs],
+  parameters: {
+    componentSubtitle: 'Result 컴포넌트',
+  },
+};
+
+export const Basics = () => {
+  return (
+    <Flex direction='column' gap={50} style={{ width: '700px' }}>
+      <Result
+        title='Empty polls !'
+        subTitle={'You can add polls by following upper steps.'}
+        status='info'
+      >
+        <div style={{ display: 'flex', gap: '10px' }}>
+          <Button size='small'>Button 1</Button>
+          <Button size='small' variant='outlined'>
+            Button 2
+          </Button>
+        </div>
+      </Result>
+    </Flex>
+  );
+};
+
+export const WithStatus = () => {
+  return (
+    <Flex direction='column' gap={50} style={{ width: '400px' }}>
+      <Result
+        title='Empty polls !'
+        subTitle={'You can add polls by following upper steps.'}
+        status='info'
+      />
+      <Result
+        title='Empty polls !'
+        subTitle={'You can add polls by following upper steps.'}
+        status='warning'
+      />
+      <Result
+        title='Empty polls !'
+        subTitle={'You can add polls by following upper steps.'}
+        status='error'
+      />
+      <Result
+        title='Empty polls !'
+        subTitle={'You can add polls by following upper steps.'}
+        status='success'
+      />
+    </Flex>
+  );
+};
---
+++ src/components/Result/Result.tsx
@@ -0,0 +1,65 @@
+import { HTMLAttributes } from 'react';
+import styled from 'styled-components';
+import Icon from '../Icon';
+
+export interface Props extends HTMLAttributes<HTMLDivElement> {
+  status?: Status;
+  title: string;
+  subTitle?: string;
+}
+
+function Result({ status = 'info', title, subTitle, children, ...rest }: Props) {
+  return (
+    <ResultWrapper {...rest}>
+      <Icon
+        name={ResultIconByStatus[status].name}
+        size={60}
+        color={ResultIconByStatus[status].color}
+        style={{ marginBottom: '30px' }}
+      />
+      <Title>{title}</Title>
+      {subTitle && <SubTitle>{subTitle}</SubTitle>}
+      {children && <ChildrenWrapper>{children}</ChildrenWrapper>}
+    </ResultWrapper>
+  );
+}
+
+export default Result;
+
+const ResultIconByStatus: Record<Status, ResultIconInfo> = {
+  info: { name: 'info-rounded', color: '#005FFC' },
+  success: { name: 'check-circle-rounded', color: '#00C021' },
+  warning: { name: 'warning', color: '#FFA825' },
+  error: { name: 'close-circle', color: '#FF464A' },
+};
+
+const ResultWrapper = styled.div`
+  background: ${({ theme }) => theme.colors.white};
+  border: 1px solid ${({ theme }) => theme.colors.grey3};
+  border-radius: 20px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin: 0;
+  padding: 80px;
+  width: inherit;
+  height: inherit;
+`;
+
+const Title = styled.div`
+  color: ${({ theme }) => theme.colors.titleActive};
+  font-size: ${({ theme }) => theme.typo.large};
+  font-weight: ${({ theme }) => theme.fontWeight.text};
+  margin-bottom: 10px;
+`;
+
+const SubTitle = styled.div`
+  color: ${({ theme }) => theme.colors.grey1};
+  font-size: ${({ theme }) => theme.typo.medium};
+  font-weight: ${({ theme }) => theme.fontWeight.light};
+`;
+
+const ChildrenWrapper = styled.div`
+  margin-top: 30px;
+`;
